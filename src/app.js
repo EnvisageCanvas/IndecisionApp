3abfa422cef706f93b01d63d412c31153d86c2e1
Shivam Shrivastava <chessfreakshivam@gmail.com>
2020-06-17 22:37:18 +0530

    Boilerplate

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 //Render the template onto the div 
-ReactDOM.render(<IndecisionApp />, document.getElementById('root'));    
+ReactDOM.render(<p>Attendance App</p>, document.getElementById('root'));    
