61f53ce93303fdfaa981f133e1ff084b9fa89ead
Shivam Shrivastava <chessfreakshivam@gmail.com>
2020-06-17 22:39:51 +0530

    Revert "Boilerplate"
    This reverts commit 3abfa422cef706f93b01d63d412c31153d86c2e1.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 //Render the template onto the div 
-ReactDOM.render(<p>Attendance App</p>, document.getElementById('root'));    
+ReactDOM.render(<IndecisionApp />, document.getElementById('root'));    
